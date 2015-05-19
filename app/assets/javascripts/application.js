a42ca05692cce6f2c940381113584f70aec381c3
David Zhang <david.zhang@wishabi.com>
2015-05-18 23:43:07 -0400

    Created page load prototype and some side bar styles

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,10 +4,26 @@
 //= require utility
 
 $(document).on('ready', function() {
-  // Scope vars
+  var $frame = $('#app-frame');
+  var $topbar = $('#app-topbar');
   var $container = $('#app-container');
+  var $menu = $('#app-topbar i.fa-bars');
+  var $expand = $('#app-topbar i.fa-expand');
 
-  $container.on('click', function() {
+  // Scroll block
+  function frameScrollHandler() {
+    console.log("hi");
+    var position = $(this).scrollTop();
+    if (position > 10) {
+      $topbar.css('background','rgba(0,0,0,0)')
+    } else {
+      $topbar.css('background','rgba(0,0,0,1)')
+    }
+  };
+  $frame[0].contentWindow.onscroll = frameScrollHandler();
+
+  // Menu block
+  $menu.on('click', function() {
     if ($container.hasClass('transformed')) {
       $container.removeClass('transformed');
       $container.velocity('reverse');
@@ -19,7 +35,34 @@ $(document).on('ready', function() {
     }
   });
 
-  // Run intro
+  $('#app-menu li').on('click', function() {
+    function switchPage() {
+      if ($frame.attr('src') == '/home')
+        $frame.attr('src','/gallery');
+      else
+        $frame.attr('src','/home');
+      $frame[0].contentWindow.onscroll = frameScrollHandler();
+    };
+
+    $('#app-body')
+        .velocity({ translateZ: '-9000px' },
+                  { duration: 1000,
+                    complete: function(){ switchPage() } })
+        .velocity({ translateZ: '0px',
+                    translateY: '-1000px'},
+                  { duration: 0 })
+        .velocity({ translateY: '0px'},
+                  { duration: 1000 });
+  });
+
+
+  // Expand block
+
+  $expand.on('click', function() {
+    fullScreen(document.querySelector('body'));
+  });
+
+  // Intro block
   $('#app-intro img').velocity(
       'fadeOut',
       { duration: 500,
@@ -30,11 +73,4 @@ $(document).on('ready', function() {
       { duration: 1500,
         delay: 1500 }
   );
-
-  $('#fullscreen').on('click', function() {
-    fullScreen(document.querySelector('body'));
-  });
-  /* TODO: AJAX Loader show/hide function with callback */
-  /* TODO: Hide menu then run AJAX to fetch page */
-  /* TODO: AJAX page switch (seems hard) */
 });
